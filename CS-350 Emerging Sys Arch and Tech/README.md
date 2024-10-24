e58d9cff3a875ebdfb0aadb60f5ec64234a0a4c3
MJSNHU <147655558+MJSNHU@users.noreply.github.com>
2024-10-24 15:38:14 -0400

    Uploaded Thermostat Project, Updated README

--- CS-350 Emerging Sys Arch and Tech/README.md
+++ CS-350 Emerging Sys Arch and Tech/README.md
@@ -1,6 +1,18 @@
 -   Summarize the project and what problem it was solving.
+	The thermostat prototype used input buttons to raise and lower the set temperature and an LED light to simulate the thermostat turning on whenever the set temperature was higher than the ambient temperature.  It used a task scheduler to check the state of the buttons, temperature reading and print data to the UART output at different intervals.  The code I wrote is in file 7-1.c  
+
 -   What did you do particularly well?
+	Designing the scheduler to loop through each task in one for loop while only triggering a given task at its individual period was a satisfying challenge. 
+
 -   Where could you improve?
+
+	The state machine I implemented essentially served as a flag, rather than progressing through states during each tick function.
+
 -   What tools and/or resources are you adding to your support network?
+	I've included a video demonstration and a flow chart to showcase how the design works.
+
 -   What skills from this project will be particularly transferable to other projects and/or course work?
+	Writing software to control hardware components, 
+
 -   How did you make this project maintainable, readable, and adaptable?
+	Writing code to initialize and execute the tasks had the same, repeatable steps which makes it easy to expand the number of tasks in the scheduler.  
---
+++ CS-350 Emerging Sys Arch and Tech/Thermostat Prototype with Task Scheduling/7-1/MCU+Image/subdir_vars.mk
@@ -0,0 +1,75 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../cc32xxs_nortos.cmd 
+
+SYSCFG_SRCS += \
+../7-1.syscfg \
+../image.syscfg 
+
+C_SRCS += \
+../7-1.c \
+./syscfg/ti_drivers_config.c \
+../main_nortos.c 
+
+GEN_FILES += \
+./syscfg/ti_drivers_config.c 
+
+GEN_MISC_DIRS += \
+./syscfg \
+./syscfg 
+
+C_DEPS += \
+./7-1.d \
+./syscfg/ti_drivers_config.d \
+./main_nortos.d 
+
+OBJS += \
+./7-1.o \
+./syscfg/ti_drivers_config.o \
+./main_nortos.o 
+
+GEN_MISC_FILES += \
+./syscfg/ti_drivers_config.h \
+./syscfg/ti_utils_build_linker.cmd.genlibs \
+./syscfg/syscfg_c.rov.xs \
+./syscfg/ti_drivers_net_wifi_config.json 
+
+GEN_MISC_DIRS__QUOTED += \
+"syscfg" \
+"syscfg" 
+
+OBJS__QUOTED += \
+"7-1.o" \
+"syscfg\ti_drivers_config.o" \
+"main_nortos.o" 
+
+GEN_MISC_FILES__QUOTED += \
+"syscfg\ti_drivers_config.h" \
+"syscfg\ti_utils_build_linker.cmd.genlibs" \
+"syscfg\syscfg_c.rov.xs" \
+"syscfg\ti_drivers_net_wifi_config.json" 
+
+C_DEPS__QUOTED += \
+"7-1.d" \
+"syscfg\ti_drivers_config.d" \
+"main_nortos.d" 
+
+GEN_FILES__QUOTED += \
+"syscfg\ti_drivers_config.c" 
+
+C_SRCS__QUOTED += \
+"../7-1.c" \
+"./syscfg/ti_drivers_config.c" \
+"../main_nortos.c" 
+
+SYSCFG_SRCS__QUOTED += \
+"../7-1.syscfg" \
+"../image.syscfg" 
+
+
